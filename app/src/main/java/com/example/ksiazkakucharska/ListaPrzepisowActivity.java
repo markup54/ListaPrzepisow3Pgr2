c8b16cf7ffa93b12a990b5d5705b9d6d6f78f037
markup54 <marzenakupka@tlen.pl>
2022-11-17 10:33:22 +0100

    wypelnienie przepisu ze szczegulami

--- app/src/main/java/com/example/ksiazkakucharska/ListaPrzepisowActivity.java
+++ app/src/main/java/com/example/ksiazkakucharska/ListaPrzepisowActivity.java
@@ -2,7 +2,10 @@ package com.example.ksiazkakucharska;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -13,6 +16,8 @@ public class ListaPrzepisowActivity extends AppCompatActivity {
 
     ArrayAdapter<Przepis> adapterPrzepisy;
     ListView listViewPrzepisy;
+    public static final String EXTRA_IDPRZEPISU = "id_przepisu";
+    public static final String EXTRA_KATEGORIA = "id_kategorii";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +33,18 @@ public class ListaPrzepisowActivity extends AppCompatActivity {
         listViewPrzepisy = findViewById(R.id.listViewPrzepisow);
         listViewPrzepisy.setAdapter(adapterPrzepisy);
 
-
+        listViewPrzepisy.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        Intent intencja = new Intent(ListaPrzepisowActivity.this,
+                                PrzepisActivity.class);
+                        intencja.putExtra(EXTRA_IDPRZEPISU,i);
+                        intencja.putExtra(EXTRA_KATEGORIA,kategoria);
+                        startActivity(intencja);
+                    }
+                }
+        );
 
     }
 }
--- app/src/main/java/com/example/ksiazkakucharska/PrzepisActivity.java
+++ app/src/main/java/com/example/ksiazkakucharska/PrzepisActivity.java
@@ -3,12 +3,31 @@ package com.example.ksiazkakucharska;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class PrzepisActivity extends AppCompatActivity {
+    ImageView imageView;
+    TextView textView;
+    TextView textViewSkladnik;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_przepis);
+        int ktoryPrzepis = getIntent().getExtras()
+                .getInt(ListaPrzepisowActivity.EXTRA_IDPRZEPISU,0);
+        int kategoria = getIntent().getExtras().getInt(ListaPrzepisowActivity.EXTRA_KATEGORIA,0);
+        Przepis przepis = RepozytoriumPrzepisow.wybierzPrzepisy(kategoria).get(ktoryPrzepis);
+        imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.textViewTytul);
+        textViewSkladnik = findViewById(R.id.textViewSkladniki);
+        imageView.setImageResource(przepis.getIdObrazka());
+        textViewSkladnik.setText(przepis.getListaSkladnikow());
+        textView.setText(przepis.getNazwa());
     }
 }
