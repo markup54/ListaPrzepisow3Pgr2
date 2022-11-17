0dce5edd724bc68793ae97c5637665cdfc04f25d
markup54 <marzenakupka@tlen.pl>
2022-11-17 11:11:03 +0100

    przycisk podziel sie wysylanie wiadomosci

--- app/src/main/java/com/example/ksiazkakucharska/PrzepisActivity.java
+++ app/src/main/java/com/example/ksiazkakucharska/PrzepisActivity.java
@@ -2,7 +2,9 @@ package com.example.ksiazkakucharska;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -29,5 +31,21 @@ public class PrzepisActivity extends AppCompatActivity {
         imageView.setImageResource(przepis.getIdObrazka());
         textViewSkladnik.setText(przepis.getListaSkladnikow());
         textView.setText(przepis.getNazwa());
+        button = findViewById(R.id.button);
+        button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        //w manifeście dodać filtr do wysyłania sms
+                        Intent intencja = new Intent();
+                        intencja.setAction(Intent.ACTION_SEND);
+                        intencja.putExtra(Intent.EXTRA_TEXT,
+                                przepis.getNazwa() +" "+przepis.getListaSkladnikow()+" Smacznego");
+                        intencja.setType("text/plain");
+                        Intent podzielSieIntencja = Intent.createChooser(intencja,null);
+                        startActivity(podzielSieIntencja);
+                    }
+                }
+        );
     }
 }
