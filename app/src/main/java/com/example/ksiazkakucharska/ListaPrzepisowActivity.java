27ac575196e6bb1d9ead69233d9dd5c908140717
markup54 <marzenakupka@tlen.pl>
2022-11-17 09:02:51 +0100

    Przejcie do listy przepisow

--- app/src/main/java/com/example/ksiazkakucharska/ListaPrzepisowActivity.java
+++ app/src/main/java/com/example/ksiazkakucharska/ListaPrzepisowActivity.java
@@ -3,6 +3,7 @@ package com.example.ksiazkakucharska;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class ListaPrzepisowActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class ListaPrzepisowActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_przepisow);
+        int kategoria = (int)getIntent().getExtras().get(MainActivity.EXTRA_KATEGORIA);
+        Toast.makeText(this, Integer.toString(kategoria), Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/ksiazkakucharska/MainActivity.java
+++ app/src/main/java/com/example/ksiazkakucharska/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ksiazkakucharska;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -10,6 +11,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     ListView listViewKategorie;
+    public static final String EXTRA_KATEGORIA = "idKategorii";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,12 +22,21 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onItemClick(AdapterView<?> adapterView,
                                             View view, int i, long l) {
-                        String kategoria = adapterView.getItemAtPosition(i).toString();
+                        wyswietlNowaAktywnosc(i);
+                        /*String kategoria = adapterView.getItemAtPosition(i).toString();
                         Toast.makeText(MainActivity.this,
                                 kategoria+" pod indeksem "+i,
                                 Toast.LENGTH_SHORT).show();
+
+                         */
                     }
                 }
         );
+    }
+
+    private void wyswietlNowaAktywnosc(int kategoria){
+        Intent intencja = new Intent(this,ListaPrzepisowActivity.class);
+        intencja.putExtra(EXTRA_KATEGORIA,kategoria);
+        startActivity(intencja);
     }
 }
