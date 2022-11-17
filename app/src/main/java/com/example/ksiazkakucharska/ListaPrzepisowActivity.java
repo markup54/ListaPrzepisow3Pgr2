e43b631b07354293ea0df9f3819e2ac32b393cc2
markup54 <marzenakupka@tlen.pl>
2022-11-17 09:42:47 +0100

    Wyswietlanie listy dynamicznie

--- app/src/main/java/com/example/ksiazkakucharska/ListaPrzepisowActivity.java
+++ app/src/main/java/com/example/ksiazkakucharska/ListaPrzepisowActivity.java
@@ -3,9 +3,16 @@ package com.example.ksiazkakucharska;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class ListaPrzepisowActivity extends AppCompatActivity {
+
+    ArrayAdapter<Przepis> adapterPrzepisy;
+    ListView listViewPrzepisy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +20,15 @@ public class ListaPrzepisowActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lista_przepisow);
         int kategoria = (int)getIntent().getExtras().get(MainActivity.EXTRA_KATEGORIA);
         Toast.makeText(this, Integer.toString(kategoria), Toast.LENGTH_SHORT).show();
+        ArrayList<Przepis> wybranePrzepisy = RepozytoriumPrzepisow.wybierzPrzepisy(kategoria);
+        adapterPrzepisy =
+                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
+                        wybranePrzepisy);
+
+        listViewPrzepisy = findViewById(R.id.listViewPrzepisow);
+        listViewPrzepisy.setAdapter(adapterPrzepisy);
+
+
+
     }
 }
